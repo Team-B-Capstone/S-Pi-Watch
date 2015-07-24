ae63aec6c2db8c46583e92727c1b1c39087aafdb
weisensee <lucas.weisensee@gmail.com>
2015-07-24 15:19:20 -0700

    GCM: added registration to MainActivity and icon for testing notification

--- mobile/src/main/java/edu/pdx/team_b_capstone2015/s_pi_watch/MainActivity.java
+++ mobile/src/main/java/edu/pdx/team_b_capstone2015/s_pi_watch/MainActivity.java
@@ -1,10 +1,14 @@
 package edu.pdx.team_b_capstone2015.s_pi_watch;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,9 +22,12 @@ import android.app.Notification;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
@@ -39,6 +46,11 @@ import java.net.URL;
 
 public class MainActivity extends Activity{
     private static final String TAG = "SPIMAIN";
+
+    // Google Cloud Messaging helper objects:
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
 
     protected void addNotification() {
         int notificationId = 001;
@@ -105,15 +117,67 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // register local device with google cloud server
+        registerWithGC();
+
         Button mButton = (Button) findViewById(R.id.button);
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 //Call the function that creates and sends the notifications
                 addNotification();
             }
         });
+    }
+
+    // registers the local device with google cloud server
+    // allows backend to send messages (alerts!)
+    protected void registerWithGC() {
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // TODO: I used to have an info display that would update when the GCM message was
+                // sent, but it's not necessary at the moment
+                // however, this abstract class has to be implemented, so I thought I'd keep the
+                // skeleton code around in case was want it later (it is kind of nice!)
+//                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+//                boolean sentToken = sharedPreferences
+//                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
+//                if (sentToken) {
+//                    mInformationTextView.setText(getString(R.string.gcm_send_message));
+//                } else {
+//                    mInformationTextView.setText(getString(R.string.token_error_message));
+//                }
+            }
+        };
+
+        if (checkPlayServices()) {
+            Log.i(TAG, "GCM: Initiating Intent: Registering with GCM");
+            // Start IntentService to register this application with GCM.
+            Intent GCMRegistration = new Intent(MainActivity.this, SpiRegistrationIntentService.class);
+            startService(GCMRegistration);
+        }
+    }
+
+    /**
+     * Check the device to make sure this device has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 
 }
