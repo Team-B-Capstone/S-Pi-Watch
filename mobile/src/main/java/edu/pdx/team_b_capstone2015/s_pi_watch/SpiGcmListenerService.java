7874500e3efc3430d606de8cf75ed69a86ca833f
Joel <jcrans2@pdx.edu>
2015-08-14 11:14:47 -0700

    Cleaned up SpiGcmListenerService

--- mobile/src/main/java/edu/pdx/team_b_capstone2015/s_pi_watch/SpiGcmListenerService.java
+++ mobile/src/main/java/edu/pdx/team_b_capstone2015/s_pi_watch/SpiGcmListenerService.java
@@ -21,19 +21,13 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.net.Uri;
-import android.opengl.Visibility;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.renderscript.RenderScript;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.gcm.GcmListenerService;
-import com.google.android.gms.wearable.DataApi;
-import com.google.android.gms.wearable.Wearable;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -55,7 +49,6 @@ public class SpiGcmListenerService extends GcmListenerService {
     private static final String TITLE = "title";
     private static final String NAME = "name";
     private static final String BED = "bed" ;
-    //private static final String GROUP_KEY = "PatientAlert";
 
     /**
      * Called when message is received.
@@ -67,15 +60,8 @@ public class SpiGcmListenerService extends GcmListenerService {
     // [START receive_message]
     @Override
     public void onMessageReceived(String from, Bundle data) {
-        //DEBUG
-/*        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
-                .build();
-        mGoogleApiClient.blockingConnect();
-        DataApi.DataItemResult databuffer = Wearable.DataApi.getDataItem(mGoogleApiClient, Uri.parse(PATH_PATIENT + 1)).await();
-                //Log.d(TAG, "Patient1 name : " +);*/
-                String message = data.getString("message");
-        Log.d(TAG, "Message: " + message);
+
+        Log.d(TAG, "Message: " + data.getString("message"));
         Map<String, String> alert;
         if (data.getString("title").contentEquals("SPI ALERT!")
                 && PreferenceManager.getDefaultSharedPreferences(this).getBoolean("pref_GCMNotifications", false)) {
@@ -87,29 +73,6 @@ public class SpiGcmListenerService extends GcmListenerService {
         }
     }
 
-    // [END receive_message]
-
-//    private void sendNotification(Map<String, String> alert) {
-//        Log.d(TAG, "sending alert, patient Id: " + alert.get(ID));
-//        Intent intent = new Intent(this, MainActivity.class);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        //PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-//        //        PendingIntent.FLAG_ONE_SHOT);
-//        //Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-//                .setSmallIcon(R.drawable.ic_stat_ic_notification)
-//                //.setVibrate(new long[]{0, 1000, 1000, 1000})
-//                .setContentTitle("ALERT")//alert.get(TITLE))
-//                .setContentText("PATIENT ID: " + alert.get(ID)
-//                        + "\nTime:" + alert.get(TS));
-//                //.setSound(defaultSoundUri)
-//                //.setContentIntent(pendingIntent);
-//
-//        NotificationManager notificationManager =
-//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//
-//        notificationManager.notify(Integer.valueOf(alert.get(ID)) /* ID of notification */, notificationBuilder.build());
-//    }
 
     //parses the json and returns a list of JSON objects each containing an alert;
     private Map<String, String> parseJSON(String result) {
@@ -135,10 +98,6 @@ public class SpiGcmListenerService extends GcmListenerService {
     private void sendNotification(Map<String, String> alert) {
         Log.d(TAG, "Sending notificaton for ID: " + alert.get(ID));
         Integer notificationId = Integer.valueOf(alert.get(ID));
-
-        // This intent cancels the queries, used when the app is closed
-        Intent launchPatientView = new Intent(this, edu.pdx.team_b_capstone2015.s_pi_watch.MainActivity.class);
-        PendingIntent launchIntent = PendingIntent.getService(this, 0, launchPatientView, PendingIntent.FLAG_CANCEL_CURRENT);
 
 
 
@@ -183,45 +142,8 @@ public class SpiGcmListenerService extends GcmListenerService {
         // Build the notification and issues it with notification manager.
         notificationManager.notify(notificationId, notificationBuilder.build());
 
-
-        // Create a big text style for the second page
-//        NotificationCompat.BigTextStyle secondPageStyle = new NotificationCompat.BigTextStyle();
-//        secondPageStyle.setBigContentTitle("Patient History")
-//                .bigText("\nExtra \nPatient \nHistory \nInformation");
-//
-
-        // Create second page notification
-//        Notification secondPageNotification =
-//                new NotificationCompat.Builder(this)
-//                        .setStyle(secondPageStyle)
-//                        .build();
-
-        //Create a big Image type for the third page
-//        Bitmap ekg = BitmapFactory.decodeResource(getResources(), R.mipmap.ekg_graph);
-//        NotificationCompat.BigPictureStyle thirdPageStyle = new NotificationCompat.BigPictureStyle();
-//        thirdPageStyle.bigPicture(ekg);
-
-        //Create third page notification
-        //The .extend line makes it so that it gets rid of the text box for the ekg graph page
-//        Notification thirdPageNotification =
-//                new NotificationCompat.Builder(this)
-//                        .setStyle(thirdPageStyle)
-//                        .extend(new NotificationCompat.WearableExtender().setHintShowBackgroundOnly(true))
-//                        .build();
-
-        // Extend the notification builder with the created pages
-//        Notification notification = notificationBuilder
-//                .extend(new NotificationCompat.WearableExtender()
-//                        .addPage(secondPageNotification)
-//                        .addPage(thirdPageNotification))
-//                .build();
-
-        // Send the notification
-        //notificationManager = NotificationManagerCompat.from(this);
-        //notificationManager.notify(notificationId, notification);
-       //NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
     }
-    //monitors for changes in the Vertx server ip preference and resends the registration key if it changes.
+
 
 }
 
